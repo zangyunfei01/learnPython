859b1a1d8d78780ec9a8d817b0992103719746b9
zangyunfei <zangyunfei@changba.com>
2019-03-19 00:23:48 +0800

    learn python

---
+++ practice.py
@@ -0,0 +1,19 @@
+# python中，
+# 变量名可以是数字、字母、下划线的组合，字母区分大小写，且不能以数字开头
+# = 是赋值语句，可以把任意数据类型赋值给变量
+a1 = 7
+print(a1)
+A1 = "str"
+print(A1)
+a2_ = 1.23
+print(a2_)
+
+# 可以给同一变量赋不同类型的值
+a1 = "str"
+print(a1)
+
+# ''和""均可表示字符串
+leijun = "I'm OK"
+goodgoodstudy = 'Learning python in "imooc" '
+print(leijun)
+print(goodgoodstudy)
