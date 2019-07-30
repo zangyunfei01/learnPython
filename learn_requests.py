c85c6a48a4274482a7dd86f92419d6e442ee6e9d
臧云飞 <zangyunfei@AFEIA-MacBook-Pro.local>
2019-07-31 03:00:18 +0800

    参数化

--- learn_requests.py
+++ learn_requests.py
@@ -3,9 +3,13 @@
 import requests
 import json
 
-url = {
-    'ruby': 'https://test-apis.520yidui.com/v2/members/info',
-    'go': 'http://test-api-go.520yidui.com/v2/members/info'
+RequestType = ['GET', 'POST', 'PUT', 'DELETE']
+
+ApiType = ['go', 'ruby']
+
+URL = {
+    'ruby': 'https://test-apis.520yidui.com/v2/members/mine',
+    'go': 'http://test-api-go.520yidui.com/v2/members/mine'
 }
 
 params = {
@@ -13,7 +17,7 @@ params = {
 }
 
 headers = {
-    'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NzQyNzQsImV4cGlyZV9hdCI6IjIwMTktMDctMzAgMDQ6NDQ6MjYgKzA4MDAifQ.74AnGxtcxwKDpqpdaPWYQP881cmmirmvchQe4MI9u2w'
+    'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NzQyNzQsImV4cGlyZV9hdCI6IjIwMTktMDctMzEgMDQ6NDQ6NDMgKzA4MDAifQ.HpF3_D3wPLKuIIbUB8uyIalqMzd0poGMxBCBwix_3gY'
 }
 
 data = {
@@ -22,30 +26,37 @@ data = {
 }
 
 
-def response(api_type):
-    if api_type == 'ruby':
-        r = requests.get(url['ruby'], params=params, headers=headers)
-        if 300 <= r.status_code < 400:
-            print('Redirect:', r.status_code)
-            return
-        elif 400 <= r.status_code < 500:
-            print('Incorrect request:', r.status_code)
-            return
-        elif r.status_code > 500:
-            print('Incorrect response:', r.status_code)
-        d = json.loads(r.content)
-        # print(type(d))
-        print(d)
-        return d
+def response(api_type, request_type):
     if api_type == 'go':
-        r = requests.get(url['go'], params=params, headers=headers)
-        d = json.loads(r.content)
-        print(d)
-        return d
+        url = URL['go']
+    elif api_type == 'ruby':
+        url = URL['ruby']
+
+    if request_type == 'GET':
+        r = requests.get(url, params=params, headers=headers)
+    elif request_type == 'POST':
+        r = requests.post(url, data=data, headers=headers)
+    print('%s api url:' % api_type, r.url)
+
+    if 200 <= r.status_code < 300:
+        print('Direct response:', r.status_code, '\n')
+    elif 300 <= r.status_code < 400:
+        print('Redirect:', r.status_code, '\n')
+        return
+    elif 400 <= r.status_code < 500:
+        print('Incorrect request:', r.status_code, '\n')
+        return
+    elif r.status_code > 500:
+        print('Incorrect response:', r.status_code, '\n')
+
+    d = json.loads(r.content)
+    # print(type(d))
+    print(d, '\n')
+    return d
 
 
 def diff():
-    d1, d2 = response('ruby'), response('go')
+    d1, d2 = response('ruby', 'GET'), response('go', 'GET')
     if len(d1) != len(d2):
         print('参数长度不一致:')
         print('第一组参数长度为：%s，第二组参数长度为：%s' % (len(d1), len(d2)))
@@ -54,30 +65,9 @@ def diff():
             if k1 == k2:
                 if d1[k1] != d2[k2]:
                     print('第一组数据为：', k1, d1[k1])
-                    print('第二组数据为：', k2, d2[k2])
+                    print('第二组数据为：', k2, d2[k2], '\n')
                     print('-----------------------')
 
 
-def response_test(request_type, api_type):
-    if request_type.get:
-        r = requests.get(url[api_type], params=params, headers=headers)
-    elif request_type.post:
-        r = requests.post(url[api_type], data=data, headers=headers)
-        if 300 <= r.status_code < 400:
-            print('Redirect:', r.status_code)
-            return
-        elif 400 <= r.status_code < 500:
-            print('Incorrect request:', r.status_code)
-            return
-        elif r.status_code > 500:
-            print('Incorrect response:', r.status_code)
-        d = json.loads(r.content)
-        # print(type(d))
-        print(d)
-        return d
-
-
-# diff()
-response('ruby')
-response('go')
-# print(requests.post(url['ruby'], data=data, headers=headers))
+# response('ruby', 'GET')
+diff()
