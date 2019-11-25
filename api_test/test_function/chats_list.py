b2fc4cb77968255f1cf4cec8b9c4721df2ceccfb
zangyunfei <zangyunfei@miliantech.com>
2019-11-26 03:04:12 +0800

    logging

--- api_test/test_function/chats_list.py
+++ api_test/test_function/chats_list.py
@@ -1,28 +1,43 @@
-#!/usr/bin/env python3
-from api_test.data_provider.headers import get_headers_by_test_account
+#!/usr/bin/python3
+
 import requests
 import json
 import ddt
+from api_test.data_provider.headers import get_headers_by_test_account
+import logging
 
-"""
-获取chats/list的喜欢我的人的所有last_msg
-"""
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+base_url = 'https://api.520yidui.com'
+path = '/v2/chats/list'
+headers = get_headers_by_test_account('18310061886', '1886')
+params = {'page': 1, 'chat_type': 'be_likeds'}
 
 
-class ChatsList:
-    def __init__(self, url, headers, params):
-        self.url = url
-        self.headers = headers
-        self.params = params
+def get_response_body():
+    try:
+        r = requests.get(url=base_url + path, headers=headers, params=params)
+        r.raise_for_status()
+        response_body = json.loads(r.text)
+        if len(response_body) == 0:
+            return logger.info('response is null')
+        else:
+            logger.info(f'response body is {response_body}')
+            return response_body
+    except requests.RequestException as e:
+        print(e)
 
-    def get_response_body(self):
-        r = requests.get(url=self.url, headers=self.headers, params=self.params)
-        return json.loads(r.text)
+
+def get_last_msg():
+    last_msg = []
+    for x in get_response_body():
+        for y, z in x.items():
+            if y == 'last_msg':
+                last_msg.append(z)
+    logger.info(f'last msg is :{last_msg}')
+    return last_msg
 
 
 if __name__ == '__main__':
-    url = 'https://api.520yidui.com/v2/chats/list'
-    headers = get_headers_by_test_account('18310061886', '1886')
-    params = {'page': 1, 'chat_type': 'be_likeds'}
-    chatslist = ChatsList(url, headers, params)
-    print(chatslist.get_response_body())
+    print(len(get_last_msg()))
